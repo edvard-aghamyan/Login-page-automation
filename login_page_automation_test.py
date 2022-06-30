ffe4bd1d0d3bdeafd96fd321f984dff23bb5f102
edvard-aghamyan <edvard.aghamyan@gmail.com>
2022-06-30 20:47:08 +0400

    remove code duplication

--- login_page_automation_test.py
+++ login_page_automation_test.py
@@ -12,91 +12,56 @@ class AsosLoginTestCases(unittest.TestCase):
         self.driver.maximize_window()
         self.verificationErrors = []
         self.accept_next_alert = True
+        self.driver.get("https://asos.com")
+        hover = ActionChains(self.driver).move_to_element(
+            self.driver.find_element(by=By.ID, value='myAccountDropdown')
+        )
+        hover.perform()
+        Wait = WebDriverWait(self.driver, 10)
+        Wait.until(EC.element_to_be_clickable((By.CLASS_NAME, '_1336dMe'))).click()
 
     def test_log_in_registered_user(self):
         driver = self.driver
-        driver.get("https://asos.com")
-        hover = ActionChains(self.driver).move_to_element(driver.find_element(by=By.ID, value='myAccountDropdown'))
-        hover.perform()
-        Wait = WebDriverWait(driver, 10)
-        Wait.until(EC.element_to_be_clickable((By.CLASS_NAME, '_1336dMe')))
-        element = driver.find_element(by=By.CLASS_NAME, value="_1336dMe")
-        element.click()
         loginLabel = driver.find_element(by=By.NAME, value="Username")
         loginLabel.send_keys("test1234@gmail.com")
         passwordLabel = driver.find_element(by=By.NAME, value="Password")
         passwordLabel.send_keys("test1234")
-        signButton = driver.find_element(by=By.ID, value="signin")
+        signButton = driver.find_element(by=By.ID, value='signin')
         signButton.click()
 
     def test_log_in_with_unregistered_user(self):
         driver = self.driver
-        driver.get("https://asos.com")
-        hover = ActionChains(self.driver).move_to_element(driver.find_element(by=By.ID, value='myAccountDropdown'))
-        hover.perform()
-        Wait = WebDriverWait(driver, 10)
-        Wait.until(EC.element_to_be_clickable((By.CLASS_NAME, '_1336dMe')))
-        element = driver.find_element(by=By.CLASS_NAME, value="_1336dMe")
-        element.click()
         loginLabel = driver.find_element(by=By.NAME, value="Username")
         loginLabel.send_keys("testmail12@gmail.com")
         passwordLabel = driver.find_element(by=By.NAME, value="Password")
         passwordLabel.send_keys("test12345")
-        signButton = driver.find_element(by=By.ID, value="signin")
+        signButton = driver.find_element(by=By.ID, value='signin')
         signButton.click()
 
     def test_log_in_with_empty_email(self):
         driver = self.driver
-        driver.get("https://asos.com")
-        hover = ActionChains(self.driver).move_to_element(driver.find_element(by=By.ID, value='myAccountDropdown'))
-        hover.perform()
-        Wait = WebDriverWait(driver, 10)
-        Wait.until(EC.element_to_be_clickable((By.CLASS_NAME, '_1336dMe')))
-        element = driver.find_element(by=By.CLASS_NAME, value="_1336dMe")
-        element.click()
         passwordLabel = driver.find_element(by=By.NAME, value="Password")
         passwordLabel.send_keys("test1234")
-        signButton = driver.find_element(by=By.ID, value="signin")
+        signButton = driver.find_element(by=By.ID, value='signin')
         signButton.click()
 
     def test_log_in_with_empty_password(self):
         driver = self.driver
-        driver.get("https://asos.com")
-        hover = ActionChains(self.driver).move_to_element(driver.find_element(by=By.ID, value='myAccountDropdown'))
-        hover.perform()
-        Wait = WebDriverWait(driver, 10)
-        Wait.until(EC.element_to_be_clickable((By.CLASS_NAME, '_1336dMe')))
-        element = driver.find_element(by=By.CLASS_NAME, value="_1336dMe")
-        element.click()
         loginLabel = driver.find_element(by=By.NAME, value="Username")
         loginLabel.send_keys("testmail@gmail.com")
-        signButton = driver.find_element(by=By.ID, value="signin")
+        signButton = driver.find_element(by=By.ID, value='signin')
         signButton.click()
 
     def test_log_in_with_empty_email_and_password(self):
         driver = self.driver
-        driver.get("https://asos.com")
-        hover = ActionChains(self.driver).move_to_element(driver.find_element(by=By.ID, value='myAccountDropdown'))
-        hover.perform()
-        Wait = WebDriverWait(driver, 10)
-        Wait.until(EC.element_to_be_clickable((By.CLASS_NAME, '_1336dMe')))
-        element = driver.find_element(by=By.CLASS_NAME, value="_1336dMe")
-        element.click()
-        signButton = driver.find_element(by=By.ID, value="signin")
+        signButton = driver.find_element(by=By.ID, value='signin')
         signButton.click()
 
     def test_check_masked_password(self):
         driver = self.driver
-        driver.get("https://asos.com")
-        hover = ActionChains(self.driver).move_to_element(driver.find_element(by=By.ID, value='myAccountDropdown'))
-        hover.perform()
-        Wait = WebDriverWait(driver, 10)
-        Wait.until(EC.element_to_be_clickable((By.CLASS_NAME, '_1336dMe')))
-        element = driver.find_element(by=By.CLASS_NAME, value="_1336dMe")
-        element.click()
         passwordLabel = driver.find_element(by=By.NAME, value="Password")
         passwordLabel.send_keys("12345678abc")
-        signButton = driver.find_element(by=By.ID, value="signin")
+        signButton = driver.find_element(by=By.ID, value='signin')
         signButton.click()
 
     def tearDown(self):
