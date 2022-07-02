0f29384e4ed7e1a0de754cb7ccd5f82238223d69
edvard-aghamyan <edvard.aghamyan@gmail.com>
2022-07-02 12:54:55 +0400

    open login page directly

--- login_page_automation_test.py
+++ login_page_automation_test.py
@@ -21,13 +21,7 @@ class AsosLoginTestCases(unittest.TestCase):
         self.driver.maximize_window()
         self.verificationErrors = []
         self.accept_next_alert = True
-        self.driver.get("https://asos.com")
-        hover = ActionChains(self.driver).move_to_element(
-            self.driver.find_element(by=By.ID, value='myAccountDropdown')
-        )
-        hover.perform()
-        Wait = WebDriverWait(self.driver, 10)
-        Wait.until(EC.element_to_be_clickable((By.CLASS_NAME, '_1336dMe'))).click()
+        self.driver.get("https://my.asos.com/identity/login")
 
     def test_log_in_registered_user(self):
         try_to_login(self.driver, "test1234@gmail.com", "test1234")
